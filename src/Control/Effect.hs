5811a3a3fbfaf2bbd69831ad17b2f60d6266f599
YellPika <yellpika@gmail.com>
2014-05-07 14:52:12 -0400

    Added Control.Effect (re-exports everything)

---
+++ src/Control/Effect.hs
@@ -0,0 +1,23 @@
+module Control.Effect (
+    module Control.Effect.Coroutine,
+    module Control.Effect.Exception,
+    module Control.Effect.Lift,
+    module Control.Effect.List,
+    module Control.Effect.Reader,
+    module Control.Effect.Resource,
+    module Control.Effect.State,
+    module Control.Effect.Thread,
+    module Control.Effect.Writer,
+    module Control.Monad.Effect
+) where
+
+import Control.Effect.Coroutine
+import Control.Effect.Exception
+import Control.Effect.Lift
+import Control.Effect.List
+import Control.Effect.Reader
+import Control.Effect.Resource
+import Control.Effect.State
+import Control.Effect.Thread
+import Control.Effect.Writer
+import Control.Monad.Effect
