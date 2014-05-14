79cb7cc5ca8e60c9f411ca488398cdb84653d9dd
YellPika <yellpika@gmail.com>
2014-05-14 16:44:53 -0400

    Updated Control.Effect

--- src/Control/Effect.hs
+++ src/Control/Effect.hs
@@ -1,4 +1,5 @@
 module Control.Effect (
+    module Control.Effect.Bracket,
     module Control.Effect.Coroutine,
     module Control.Effect.Exception,
     module Control.Effect.Lift,
@@ -7,10 +8,12 @@ module Control.Effect (
     module Control.Effect.State,
     module Control.Effect.Thread,
     module Control.Effect.Union,
+    module Control.Effect.Witness,
     module Control.Effect.Writer,
     module Control.Monad.Effect
 ) where
 
+import Control.Effect.Bracket
 import Control.Effect.Coroutine
 import Control.Effect.Exception
 import Control.Effect.Lift
@@ -19,5 +22,6 @@ import Control.Effect.Reader
 import Control.Effect.State
 import Control.Effect.Thread
 import Control.Effect.Union
+import Control.Effect.Witness
 import Control.Effect.Writer
 import Control.Monad.Effect
